feed8b5b672b220f0d60b9b3e602447ce996afa4
Niru Maheswaranathan <nirum@stanford.edu>
2013-11-27 02:05:07 -0800

    updating to angular 1.2.2

--- bet-the-bill/main.js
+++ bet-the-bill/main.js
@@ -2,6 +2,7 @@ var app = angular.module('bet-the-bill', []);
 
 function DinerCtrl($scope) {
 
+   // Initialize
    $scope.showResult = false;
    $scope.diners = [{
       'name': 'Diner 1',
@@ -10,66 +11,74 @@ function DinerCtrl($scope) {
       'name': 'Diner 2',
       'amount': 0,
    }];
-	 $scope.nextDiner = 3;
-
-	 $scope.showForm = true;
-	 $scope.getTotal = function() {
-			 var total = 0;
-			 for (var i = 0; i < $scope.diners.length; i++) {
-					 if ($scope.diners[i].amount.length > 0) {
-								total += Math.ceil(100*parseFloat($scope.diners[i].amount));
-					 }
-			 }
-			 return total/100;
-	 }
+   $scope.nextDiner = 3;
+   $scope.total = 0
+   $scope.mydiner = {
+      'name': 'Eliza Orlins'
+   }
+   $scope.showForm = true;
 
    $scope.addDiner = function() {
       $scope.diners.push({
          'name': 'Diner ' + $scope.nextDiner,
          'amount': 0
       });
-			$scope.nextDiner++;
+      $scope.nextDiner++;
+   }
+
+   // compute the total amount
+   $scope.total = function() {
+      var t = 0;
+      angular.forEach($scope.diners, function(d) {
+         t += d.amount;
+      });
+      return Math.ceil(parseFloat(t) * 100) / 100
    }
 
    $scope.removeDiner = function(idx) {
-      $scope.diners.splice(idx,1);
+      $scope.diners.splice(idx, 1);
    }
 
-	 $scope.betBill = function() {
+   $scope.betBill = function() {
 
-			 // hide the form
-			 $scope.showForm = false;
+      // hide the form
+      $scope.showForm = false;
 
-			 // show the animation
+      // show the animation
+      // compute result
+      var total = $scope.total(); // get total
+      var die = Math.random() * total; // roll a die
+      var cumTotal = 0;
+      for (var i = 0; i < $scope.diners.length; i++) {
 
-			 // compute result
-			 var total = $scope.getTotal(); 						// get total
-			 var die = Math.random()*total; 						// roll a die
-			 var cumTotal = 0;
-			 for (var i = 0; i < $scope.diners.length; i++) {
+         // add this guy to cumulative total
+         cumTotal += Math.ceil(100 * parseFloat($scope.diners[i].amount)) / 100;
 
-					 // add this guy to cumulative total
-					 cumTotal += Math.ceil(100*parseFloat($scope.diners[i].amount))/100;
+         // check if this guy has to pay
+         if (die < cumTotal) {
+            $scope.setLoser(i);
+            break;
+         }
+      }
+   }
 
-					 // check if this guy has to pay
-					 if (die < cumTotal) {
-							 $scope.setLoser(i);
-							 break;
-					 }
-			 }
-	 }
+   $scope.setLoser = function(idx) {
+      $scope.loser = $scope.diners[idx];
+      $scope.showResult = true;
+   }
 
-	 $scope.setLoser = function(idx) {
-			 $scope.loser = $scope.diners[idx];
-			 $scope.showResult = true;
-	 }
+   $scope.goBack = function() {
+      $scope.showForm = true;
+      $scope.showResult = false;
+   }
+   $scope.startOver = function() {
+      $scope.showForm = true;
+      $scope.showResult = false;
+   }
 
-	 $scope.goBack = function() { $scope.showForm = true; $scope.showResult = false; }
-	 $scope.startOver = function() { $scope.showForm = true; $scope.showResult = false; }
-
-	 // helper
-	 $scope.parseAmt = function(amt) {
-			 return Math.ceil(parseFloat(amt)*100)/100;
-	 };
+   // helper
+   $scope.parseAmt = function(amt) {
+      return Math.ceil(parseFloat(amt) * 100) / 100;
+   };
 
 }
