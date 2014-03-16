18dabef9ed94ceac6d939b19f564669fb9c5c468
Niru Maheswaranathan <nirum@stanford.edu>
2014-03-16 13:33:48 -0700

    integration mostly complete

--- bet-the-bill/main.js
+++ bet-the-bill/main.js
@@ -6,16 +6,13 @@ function DinerCtrl($scope) {
    $scope.showResult = false;
    $scope.diners = [{
       'name': 'Diner 1',
-      'amount': 0,
+      'amount': 1,
    }, {
       'name': 'Diner 2',
-      'amount': 0,
+      'amount': 1,
    }];
    $scope.nextDiner = 3;
    $scope.total = 0
-   $scope.mydiner = {
-      'name': 'Eliza Orlins'
-   }
    $scope.showForm = true;
 
    $scope.addDiner = function() {
@@ -24,6 +21,7 @@ function DinerCtrl($scope) {
          'amount': 0
       });
       $scope.nextDiner++;
+			$scope.updateChart()
    }
 
    // compute the total amount
@@ -35,8 +33,9 @@ function DinerCtrl($scope) {
       return Math.ceil(parseFloat(t) * 100) / 100
    }
 
-   $scope.removeDiner = function(idx) {
+		$scope.removeDiner = function(idx) {
       $scope.diners.splice(idx, 1);
+			$scope.updateChart()
    }
 
    $scope.betBill = function() {
@@ -80,5 +79,119 @@ function DinerCtrl($scope) {
    $scope.parseAmt = function(amt) {
       return Math.ceil(parseFloat(amt) * 100) / 100;
    };
+
+		//var amounts = function() {
+				//var temp = []
+				//angular.forEach($scope.diners, function(d) {
+						//temp.push(d.amount);
+				//});
+				//return temp;
+		//}
+
+	 // Initialize d3 plot
+		var w = 400,                       // width and height, natch
+				h = 400,
+				r = Math.min(w, h) / 2,        // arc radius
+				dur = 1000,                     // duration, in milliseconds
+				color = d3.scale.category10(),
+				donut = d3.layout.pie().sort(null),
+				arc = d3.svg.arc().innerRadius(0).outerRadius(r - 20);
+
+		// ---------------------------------------------------------------------
+		var svg = d3.select("#pie-chart").append("svg:svg")
+				.attr("width", w).attr("height", h);
+
+		var arc_grp = svg.append("svg:g")
+				.attr("class", "arcGrp")
+				.attr("transform", "translate(" + (w / 2) + "," + (h / 2) + ")");
+
+		var label_group = svg.append("svg:g")
+				.attr("class", "lblGroup")
+				.attr("transform", "translate(" + (w / 2) + "," + (h / 2) + ")");
+
+		// GROUP FOR CENTER TEXT
+		//var center_group = svg.append("svg:g")
+				//.attr("class", "ctrGroup")
+				//.attr("transform", "translate(" + (w / 2) + "," + (h / 2) + ")");
+				//
+
+		// get array of diner amounts
+		var amounts = function() {
+				var temp = new Array;
+				for(var idx in $scope.diners) {
+						temp.push($scope.diners[idx].amount);
+				}
+				return temp
+		}
+
+		// DRAW ARC PATHS
+		var arcs = arc_grp.selectAll("path")
+				.data(donut(amounts()));
+		arcs.enter().append("svg:path")
+				.attr("stroke", "white")
+				.attr("stroke-width", 0.5)
+				.attr("fill", function(d, i) {return color(i);})
+				.attr("d", arc)
+				.each(function(d) {this._current = d});
+
+		// DRAW SLICE LABELS
+		var sliceLabel = label_group.selectAll("text")
+				.data(donut(amounts()));
+		sliceLabel.enter().append("svg:text")
+				.attr("class", "arcLabel")
+				.attr("x", r-100)
+				.attr("y", 0)
+				.attr("transform", function(d) {
+								var coordinates = arc.centroid(d);
+								var rotationangle = Math.atan(coordinates[1]/coordinates[0])*180/3.1415926;
+								if(coordinates[0] < 0) { rotationangle = rotationangle-180; }
+								return "rotate(" + rotationangle + ")"; })
+				.attr("text-anchor", "middle")
+				.attr("alignment-baseline", "middle")
+				.attr("style", function(d) {
+												if(d.data == 0) { return "font-size: 0;"}
+												else { return "font-size: 32px;"};})
+				.text(function(d, i) {return $scope.diners[i].name; });
+
+		// --------- "PAY NO ATTENTION TO THE MAN BEHIND THE CURTAIN" ---------
+
+		// Store the currently-displayed angles in this._current.
+		// Then, interpolate from this._current to the new angles.
+		function arcTween(a) {
+				var i = d3.interpolate(this._current, a);
+				this._current = i(0);
+				return function(t) {
+						return arc(i(t));
+				};
+		}
+
+		// update chart
+		$scope.updateChart = function() {
+
+				console.log('updating!')
+
+				myAmounts = amounts()
+
+				arcs.data(donut(myAmounts)); // recompute angles, rebind data
+				arcs.transition().ease("elastic").duration(dur).attrTween("d", arcTween);
+
+				sliceLabel.data(donut(myAmounts));
+				sliceLabel.transition().ease("elastic").duration(dur)
+				.attr("x", r-100)
+				.attr("y", 0)
+				.attr("transform", function(d) {
+								var coordinates = arc.centroid(d);
+								var rotationangle = Math.atan(coordinates[1]/coordinates[0])*180/3.1415926;
+								if(coordinates[0] < 0) { rotationangle = rotationangle-180; }
+								return "rotate(" + rotationangle + ")"; })
+				.attr("text-anchor", "middle")
+				.attr("alignment-baseline", "middle")
+				.attr("style", function(d) {
+												if(d.data == 0) { return "font-size: 0;"}
+												else { return "font-size: 32px;"};})
+				.text(function(d, i) {return $scope.diners[i].name; });
+						
+				//pieLabel.text(data.label);
+		}
 
 }
